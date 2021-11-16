933dc60a770d286b40c89d6546a3e1ee8225f320
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-11-16 15:18:27 +0200

    next

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,60 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
 console.log(galleryItems);
+
+const divGallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(function ({ preview, original, description}) {
+return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+}
+).join("");
+
+divGallery.insertAdjacentHTML("beforeend", markup);
+
+let instance;
+
+function onEscPress (event) {
+                      
+      if (event.key === 'Escape') {
+
+        instance.close();
+
+        document.removeEventListener('keydown', onEscPress);
+                        
+      };
+
+};
+   
+const onImageClick = function (event) {
+
+      if (event.target.nodeName === 'IMG') {
+        
+          event.preventDefault();
+          
+          instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`);
+                  
+          instance.show();
+
+          document.addEventListener('keydown', onEscPress);
+          
+          return;
+
+      };
+
+document.removeEventListener('keydown', onEscPress);
+
+};
+
+window.addEventListener('click', onImageClick);
